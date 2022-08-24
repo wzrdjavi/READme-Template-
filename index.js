1e1b8a9824b68c0b70b3e493533f7229a5e003a1
wzrdjavi <jmaster112605@gmail.com>
2022-08-24 14:36:55 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown")
+// TODO: Create an array of questions for user input
+
+const questions = [
+    {
+        type: "input",
+        name:"github",
+        message: "Whats your Github username?"
+    },
+    {
+        type:"input",
+        name: "email",
+        message: "What's your email?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What's the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe about your project:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license does your project have?",
+        choices: [ "APACHE2.0", "Boost1.0", "BSD2" , "MIT", "None"]
+    },
+    {
+        type: "input",
+        name:"dependencies",
+        message:"Any dependencies to install?",
+        default: "npm i"
+    },
+    {
+        type:"input",
+        name:"test",
+        message:"What command should be run to run test?",
+        default:"npm test"
+    },
+    {
+        type:"input",
+        name:"usage",
+        message:"What does the usage of this repo?"
+    },
+    {
+        type:"input",
+        name:"contributors",
+        message: "Who are the contributers of this repo?"
+    }
+]
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerAnswers) => {
+        console.log("Generating.... Please Wait....");
+       writeToFile("dist\README.md", generateMarkdown({...inquirerAnswers}));
+    })
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,61 @@
+
+
+function generateMarkdown(data) {
+    return `# ${data.title}
+  ${renderBadge(data.license)}
+  ## Github URL
+  [${data.github}](https://github.com/${data.github}/)
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installations](#dependencies)
+  * [Usage](#usage)
+  ${renderLink(data.license)}
+  * [Contributors](#contributors)
+  * [Test](#test)
+  ## Installations (Dependencies)
+  To install dependencies, run these commands:
+  \`\`\`
+  ${data.dependencies}
+  \`\`\`
+  ## Usage
+  ${data.usage}
+  ${renderSection(data.license)}
+  ## Contributors
+  ${data.contributors}
+  Contact me at ${data.email}
+  ## Tests
+  To run tests, run these commands:
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  `;
+  }
+  // Function to render badge
+  function renderBadge(license) {
+    if (license !== "None") {
+      return `![GitHub license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+    }
+    return ''
+  }
+  // Function to render link
+  function renderLink(license) {
+    if (license !== "None") {
+      return (
+        `\n* [License](#license)\n`
+      )
+    }
+    return ''
+  }
+  // Function to render section
+  function renderSection(license) {
+    if (license !== "None") {
+      return (
+        `## License
+        Copyright © ${license}. All rights reserved.
+        Licensed under the ${license} license.`
+      )
+    }
+    return ''
+  }
+  module.exports = generateMarkdown;
